05723b70e1875808bcf327be60e01eb7eddc5643
Daniel Aguilar <dacmail@gmail.com>
2019-03-05 10:26:39 +0100

    widget embed js

---
+++ public/widget.js
@@ -0,0 +1,24 @@
+(function (window, undefined) {
+  let Poletika = {};
+  if (window.Poletika) {
+    return;
+  }
+
+  function setupIframe() {
+    let target = document.getElementById('poletika-widget');
+    let iframe = document.createElement('iframe');
+    iframe.src = 'http://localhost:8081/';
+    iframe.style.width = '100%';
+    iframe.style.maxWidth = '300px';
+    iframe.style.height = '100%';
+    iframe.style.maxHeight = '500px';
+    iframe.style.border = 'none';
+    target.parentNode.insertBefore(iframe, target);
+  }
+
+  Poletika.init = function () {
+    setupIframe();
+  };
+  window.Poletika = Poletika;
+  Poletika.init();
+})(this);
