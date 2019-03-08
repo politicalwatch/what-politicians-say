a0b4c53486e73a6f462387f8c40592345f7567ea
Daniel Aguilar <dacmail@gmail.com>
2019-03-08 12:11:21 +0100

    display fixes

--- public/embed-src.js
+++ public/embed-src.js
@@ -1,19 +1,20 @@
 (function (window, undefined) {
-  let Poletika = {};
+  var Poletika = {};
   if (window.Poletika) {
     return;
   }
 
   function setupIframe() {
-    let target = document.getElementById('poletika-widget');
-    let iframe = document.createElement('iframe');
+    var target = document.getElementById('poletika-widget');
+    var iframe = document.createElement('iframe');
     iframe.src = '//widget.what-politicians-say.poletika.org/widget.html#' + target.dataset.params;
     iframe.style.width = '100%';
     iframe.style.minWidth = '300px';
-    iframe.style.maxWidth = '600px';
     iframe.style.height = '100%';
     iframe.style.maxHeight = '1000px';
     iframe.style.minHeight = '500px';
+    iframe.style.margin = '0 auto';
+    iframe.style.display = 'block';
     iframe.style.border = 'none';
     target.parentNode.insertBefore(iframe, target);
   }
